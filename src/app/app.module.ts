2821ff1cd6d9fe9c85152a3129ed37842bb1c0e9
ciemny <carole.ciemny.devweb@gmail.com>
2020-02-12 16:43:06 +0100

    commit avant branche

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DestinationComponent } from './destination/destination.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DestinationComponent
   ],
   imports: [
     BrowserModule
