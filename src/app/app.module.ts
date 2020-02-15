c1c069c6e2cb65b906ffe80d475e0cb559dc8ab2
Pierre <pierre.jolivet.devweb@gmail.com>
2020-02-15 11:29:16 +0100

    Add routing and initialize payment page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing';
 
 import { AppComponent } from './app.component';
 import { DestinationComponent } from './destination/destination.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DestinationComponent
+    DestinationComponent,
+    PaymentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
